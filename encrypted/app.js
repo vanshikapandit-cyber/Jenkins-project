5e1b000e904c8351864d9aebfbfeb9e1d6142e8a
vanshikapandit-cyber <vanshikapandit@globussoft.in>
2026-05-05 11:56:33 +0530

    removed the encrypted folder

--- encrypted/app.js
+++
@@ -1,2 +0,0 @@
-/* Protected by EmpMonitor Obfuscator | 2026-05-05 */
-(function(){var _0x=atob('Y29uc3QgZXhwcmVzcyA9IHJlcXVpcmUoJ2V4cHJlc3MnKTsKY29uc3QgYXBwID0gZXhwcmVzcygpOwphcHAuZ2V0KCcvJywgKHJlcSwgcmVzKT0+ewpyZXMuc2VuZCgiSGVsbG8gZnJvbSBicm93c2VyLCBJIGFtIHZhbnNoaWthIikKfSk7CmFwcC5saXN0ZW4oMzAwMCwoKT0+CnsKRU5DX0xPRygic2VydmVyIGlzIHJ1bm5pbmcgaW4gcG9ydCAzMDAwIik7Cn0pOw==');eval(_0x);})();
