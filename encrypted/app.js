d97d899a17369335a9504d92c00196dd5e296b6d
jenkins <jenkins@gmail.com>
2026-05-05 10:03:13 +0530

    Encrypted code

---
+++ encrypted/app.js
@@ -0,0 +1,2 @@
+/* Protected by EmpMonitor Obfuscator | 2026-05-05 */
+(function(){var _0x=atob('Y29uc3QgZXhwcmVzcyA9IHJlcXVpcmUoJ2V4cHJlc3MnKTsKY29uc3QgYXBwID0gZXhwcmVzcygpOwphcHAuZ2V0KCcvJywgKHJlcSwgcmVzKT0+ewpyZXMuc2VuZCgiSGVsbG8gZnJvbSBicm93c2VyIikKfSk7CmFwcC5saXN0ZW4oMzAwMCwoKT0+CnsKY29uc29sZS5sb2coInNlcnZlciBpcyBydW5uaW5nIGluIHBvcnQgMzAwMCIpOwp9KTs=');eval(_0x);})();
